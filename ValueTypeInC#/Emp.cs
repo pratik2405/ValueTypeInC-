3c9cf0cce42ce7002b462ae2229016af22a2da54
pratik2405 <shinde7388pratik@gmail.com>
2024-11-13 16:02:21 +0530

    Add project files.

---
+++ ValueTypeInC#/Emp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeInC_
+{
+    /*Enum=: An enumeration type is a value type defined by the a set of named 
+     *       constants of underlying integral numeric type
+*/
+
+//    To create a set of constants in C# we can use enum
+//    It is a value type
+//    Compiler will generate the set of integers constants for enum type
+
+    public enum Department {HR=1 , Sales , Dev ,Testing, Admin };
+    public enum Role {Emp=101,Manager,HRManager,CEO}
+    public class Emp
+    {
+      private int id;
+        private string name;
+        private Department dept;
+        private Role role;
+
+       public Emp(int id, string name, Department dept,Role role)
+        {
+            this.id = id;
+            this.name = name;
+            this.dept = dept;
+            this.role = role;
+        }
+
+        public override string ToString()
+        {
+            return $"{id} , {name} , {dept} , {role} ";
+        }
+    }
+}
---
+++ ValueTypeInC#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e1=new Emp(1,"Pratik",Department.HR,Role.CEO);
+            Console.WriteLine(e1);
+        }
+    }
+}
